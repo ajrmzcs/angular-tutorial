54580ce52bc19053e360d3457bb71b8a6d485668
AnRamirez <anramirez@nolo.com>
2017-08-07 13:39:13 -0500

    Ending of Spotify and Beginning of Wishes

--- 4.SpotiApp/src/app/services/spotify.service.ts
+++ 4.SpotiApp/src/app/services/spotify.service.ts
@@ -1,21 +1,63 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class SpotifyService {
 
-  artistas: any [];
+  artists: any [] = [];
 
   searchUrl: string = "https://api.spotify.com/v1/search";
+  artistUrl: string = "https://api.spotify.com/v1/artists";
 
 
-  constructor() { }
+  constructor( private http:Http ) { }
 
-  getArtistas( term:string ){
+  getArtists ( term:string ){
 
-    let query = "q=Ricardo+Montaner"
+    let headers = new Headers();
+    headers.append( 'authorization','Bearer BQCZm9Xl1HIP-GgziOJYpYo61XEVlKul4voZJkTXzaCD2o6Hh8zApcej7X7j2KFHvDMHFKOJcb9Wvjz3h7pJLA' );
 
-    let url = this.searchUrl +
+    let query =  `?q=${ term }&type=artist`;
+    let url = this.searchUrl + query;
 
+    return this.http.get( url, { headers } )
+            .map( res => {
+                // console.log( res.json().artists.items );
+                this.artists = res.json().artists.items;
+                console.log(this.artists);
+
+            })
+
+  }
+
+  getArtist ( id:string ){
+
+    let headers = new Headers();
+    headers.append( 'authorization','Bearer BQCZm9Xl1HIP-GgziOJYpYo61XEVlKul4voZJkTXzaCD2o6Hh8zApcej7X7j2KFHvDMHFKOJcb9Wvjz3h7pJLA' );
+
+    let query =  `/${ id }`;
+    let url = this.artistUrl + query;
+
+    return this.http.get( url, { headers } )
+            .map( res => {
+                return res.json();
+            })
+
+  }
+  getTop ( id:string ){
+
+    let headers = new Headers();
+    headers.append( 'authorization','Bearer BQCZm9Xl1HIP-GgziOJYpYo61XEVlKul4voZJkTXzaCD2o6Hh8zApcej7X7j2KFHvDMHFKOJcb9Wvjz3h7pJLA' );
+
+    let query =  `/${ id }/top-tracks?country=US`;
+    let url = this.artistUrl + query;
+
+    return this.http.get( url, { headers } )
+            .map( res => {
+                console.log( res.json().tracks );
+                return res.json().tracks;
+            })
 
   }
 
